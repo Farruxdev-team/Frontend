0bd1a8cce63c0d17af3f241324aec8493f030bb2
FarruxDEV <farruxbeknozimov@gmail.com>
2023-06-24 11:43:14 +0500

    changed

--- src/views/AddTeacher.vue
+++ src/views/AddTeacher.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
     <h1>Add teacher</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non enim provident nisi
+      voluptatibus, ipsum labore reprehenderit autem itaque eius necessitatibus! Amet quas iusto
+      impedit adipisci enim in. Recusandae, ratione culpa?
+    </p>
   </div>
 </template>
 
