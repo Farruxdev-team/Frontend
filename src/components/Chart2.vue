f94cd7614118d8a3c446e79afcf27e1306bc068e
Azizxon <muzaffarovazizkhon@gmail.com>
2023-06-27 09:16:27 +0500

    Stats added

---
+++ src/components/Chart2.vue
@@ -0,0 +1,39 @@
+<template>
+    <CanvasJSChart :options="options" :styles="styleOptions"/>
+</template>
+
+<script setup>
+import { reactive } from "vue"
+
+const options = reactive({
+          theme: "light2",
+          animationEnabled: true,
+          exportEnabled: true,
+          title:{
+            text: "O'zbekistonda etnik guruhlar"
+          },
+          data: [{
+            type: "pie",
+            indexLabel: "{label} (#percent%)",
+            yValueFormatString: "#,##0",
+            toolTipContent: "<span style='\"'color: {color};'\"'>{label}</span> {y}(#percent%)",
+            dataPoints: [
+              { label: "O'zbeklar", y: 29194071 },
+              { label: "Tojiklar", y: 1657336 },
+              { label: "Qozoqlar", y: 821172 },
+              { label: "Qoraqalpoqlar", y: 752646 },
+              { label: "Ruslar", y: 720324 },
+              { label: "Qirg'izlar", y: 291628 },
+              { label: "Boshqa", y: 1121714 }
+            ]
+          }]
+        })
+const styleOptions = reactive({
+          width: "100%",
+          height: "360px"
+        })
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,28 +1,7 @@
 <script setup>
 import { onMounted, ref } from "vue";
-import Chart from "../components/Chart.vue"
-import { useHome } from "../service/home/index"
-import { homeStore } from "../stores/home/homeStore"
-
-const store = homeStore()
-
-const listUpdate = () => {
-  useHome.list_students().then((res) => {
-    store.SET_LIST_STUDENTS(res?.data)
-  });
-  useHome.list_staff().then((res) => {
-    store.SET_LIST_STAFF(res?.data)
-  });
-  useHome.list_groups().then((res) => {
-    store.SET_LIST_GROUPS(res?.data)
-  });
-  useHome.list_tests().then((res) => {
-    store.SET_LIST_TESTS(res?.data)
-  });
-  useHome.list_subjects().then((res) => {
-    store.SET_LIST_SUBJECTS(res?.data)
-  });
-}
+import Chart1 from "../components/Chart1.vue"
+import Chart2 from "../components/Chart2.vue"
 
 onMounted(() => {
   listUpdate();
@@ -30,13 +9,10 @@ onMounted(() => {
 </script>
 
 <template>
-  <Chart :data="{
-    students: store.LIST_STUDENTS.length,
-    staff: store.LIST_STAFF.length,
-    groups: store.LIST_GROUPS.length,
-    tests: store.LIST_TESTS.length,
-    subjects: store.LIST_SUBJECTS.length
-  }"/>
+  <div style="display: grid; grid-template-columns: 48% 48%;" class="gap-9">
+    <Chart1 />
+    <Chart2 />
+  </div>
 </template>
 
 
