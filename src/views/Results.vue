4ce073e680dad18da5a3c728ebbda919f456a661
FarruxDEV <farruxbeknozimov@gmail.com>
2023-07-06 23:23:47 +0500

    change on results page

--- src/views/Results.vue
+++ src/views/Results.vue
@@ -22,32 +22,52 @@ onMounted(async () => {
 
 <template>
   <div
-    class="w-full bg-white border border-gray-400 dark:bg-gray-800 dark:border-gray-600 p-4 rounded-xl mb-5 shadow-lg"
+    class="w-full bg-white border border-gray-400 dark:bg-gray-800 dark:border-gray-600 p-4 py-14 rounded-xl mb-5 shadow-lg"
   >
     <div v-if="load" class="w-full flex items-center justify-center py-20">
       <LoadingSpinner />
     </div>
     <div v-else>
       <div v-if="!userResults.length">
-        <h4 class="text-white text-lg text-center">NO DATA</h4>
+        <h4 class="dark:text-white text-lg text-center">NO DATA</h4>
       </div>
-      <div v-else class="grid lg:grid-cols-3 md:grid-cols-2 gap-5">
-        <div v-for="el in userResults" class="dark:bg-gray-900 bg-gray-300 rounded-lg px-5 py-2">
-          <canvas
-            data-te-chart="pie"
-            data-te-dataset-label="Traffic"
-            data-te-labels="['Correct', 'Incorrect']"
-            data-te-dataset-data="[20, 10]"
-            data-te-dataset-background-color="['rgb(55, 255, 0, 0.7)', 'rgb(255, 17, 0, 0.7)']"
+      <div v-else class="grid grid-cols-1 gap-10 lg:grid-cols-2 lg:gap-10">
+        <div
+          v-for="el in userResults"
+          class="mb-5 flex items-center flex-wrap max-w-md px-10 dark:bg-gray-900 bg-gray-300 shadow hover:shadow-xl cursor-pointer rounded-2xl h-20"
+          x-data="{ circumference: 50 * 2 * Math.PI, percent: 80 }"
+        >
+          <div
+            class="relative flex items-center justify-center -m-6 overflow-hidden dark:bg-gray-900 bg-gray-300 rounded-full"
           >
-          </canvas>
-          <router-link
-            :to="`results/${el._id}`"
-            class="text-sm dark:bg-gray-800 bg-white dark:text-white text-center my-2 rounded-lg p-1 flex items-center justify-center gap-2"
-          >
-            {{ el?.test_group_id?.name }}
-            <i class="bx bx-right-arrow-alt text-lg"></i>
-          </router-link>
+            <svg class="w-32 h-32 transform translate-x-1 translate-y-1" x-cloak aria-hidden="true">
+              <circle
+                class="text-gray-300"
+                stroke-width="10"
+                stroke="currentColor"
+                fill="transparent"
+                r="50"
+                cx="60"
+                cy="60"
+              />
+              <circle
+                class="text-blue-600"
+                stroke-width="10"
+                :stroke-dasharray="circumference"
+                :stroke-dashoffset="circumference - (percent / 100) * circumference"
+                stroke-linecap="round"
+                stroke="currentColor"
+                fill="transparent"
+                r="50"
+                cx="60"
+                cy="60"
+              />
+            </svg>
+            <span class="absolute text-2xl text-blue-700" x-text="`${percent}%`"></span>
+          </div>
+          <p class="ml-10 font-medium text-gray-600 text-md overflow-x-auto">
+            {{ el.test_group_id.name }}
+          </p>
         </div>
       </div>
     </div>
