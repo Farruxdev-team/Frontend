6d7dfcc87e9026d595598e356cf76dfa7048de90
Ozodbek <ozodbekhimmatov@gmail.com>
2023-07-06 15:34:07 +0500

    Al-Wakeel

--- src/components/Chart2.vue
+++ src/components/Chart2.vue
@@ -1,37 +1,37 @@
 <script setup>
-import { reactive, ref } from 'vue'
-import { useModeStore } from '@/stores/mode'
-const store = useModeStore()
+import { reactive, ref } from "vue";
+import { useModeStore } from "@/stores/mode";
+const store = useModeStore();
 console.log(store.isDark);
 const options = reactive({
-  theme: store.isDark ? 'dark2' : 'light2',
+  theme: store.isDark ? "dark2" : "light2",
   animationEnabled: true,
   exportEnabled: true,
   title: {
-    text: "O'zbekistonda etnik guruhlar (2021)"
+    text: "O'zbekistonda etnik guruhlar (2021)",
   },
   data: [
     {
-      type: 'pie',
-      indexLabel: '{label} (#percent%)',
-      yValueFormatString: '#,##0',
+      type: "pie",
+      indexLabel: "{label} (#percent%)",
+      yValueFormatString: "#,##0",
       toolTipContent: "<span style='\"'color: {color};'\"'>{label}</span> {y}(#percent%)",
       dataPoints: [
         { label: "O'zbeklar", y: 29194071 },
-        { label: 'Tojiklar', y: 1657336 },
-        { label: 'Qozoqlar', y: 821172 },
-        { label: 'Qoraqalpoqlar', y: 752646 },
-        { label: 'Ruslar', y: 720324 },
+        { label: "Tojiklar", y: 1657336 },
+        { label: "Qozoqlar", y: 821172 },
+        { label: "Qoraqalpoqlar", y: 752646 },
+        { label: "Ruslar", y: 720324 },
         { label: "Qirg'izlar", y: 291628 },
-        { label: 'Boshqa', y: 1121714 }
-      ]
-    }
-  ]
-})
+        { label: "Boshqa", y: 1121714 },
+      ],
+    },
+  ],
+});
 const styleOptions = reactive({
-  width: '100%',
-  height: '360px'
-})
+  width: "100%",
+  height: "360px",
+});
 </script>
 
 <template>
