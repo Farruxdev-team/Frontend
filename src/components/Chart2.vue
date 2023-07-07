14064714159b6f78edb6f85e85b1406e37734cf5
FarruxDEV <farruxbeknozimov@gmail.com>
2023-07-07 20:14:41 +0500

    change chart

--- src/components/Chart2.vue
+++ src/components/Chart2.vue
@@ -1,73 +1,81 @@
 <script setup>
-import { onMounted } from "vue";
+import { onMounted } from 'vue'
+import { studentStore } from '../stores/students/studentStore'
+import moment from 'moment'
 
-import Loading from "@/components/Loading.vue";
+const students_store = studentStore()
 
-import { Chart, initTE } from "tw-elements";
+import { Chart, initTE } from 'tw-elements'
 
 const dataBarCustomOptions = {
-  type: "bar",
+  type: 'bar',
   data: {
-    labels: ["January", "February", "March", "April", "May", "June"],
+    labels: [
+      moment(new Date().setDate(new Date().getDate() - 4)).format('dddd'),
+      moment(new Date().setDate(new Date().getDate() - 3)).format('dddd'),
+      moment(new Date().setDate(new Date().getDate() - 2)).format('dddd'),
+      moment(new Date().setDate(new Date().getDate() - 1)).format('dddd'),
+      moment(new Date().setDate(new Date().getDate())).format('dddd')
+    ],
     datasets: [
       {
-        label: "Traffic",
-        data: [30, 15, 62, 65, 61, 6],
+        label: 'Traffic',
+        data: [1, 5, 1, 10, 7],
         backgroundColor: [
-          "rgba(255, 99, 132, 0.2)",
-          "rgba(54, 162, 235, 0.2)",
-          "rgba(255, 206, 86, 0.2)",
-          "rgba(75, 192, 192, 0.2)",
-          "rgba(153, 102, 255, 0.2)",
-          "rgba(255, 159, 64, 0.2)",
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
         ],
         borderColor: [
-          "rgba(255,99,132,1)",
-          "rgba(54, 162, 235, 1)",
-          "rgba(255, 206, 86, 1)",
-          "rgba(75, 192, 192, 1)",
-          "rgba(153, 102, 255, 1)",
-          "rgba(255, 159, 64, 1)",
+          'rgba(255,99,132,1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
         ],
-        borderWidth: 1,
-      },
-    ],
-  },
-};
+        borderWidth: 1
+      }
+    ]
+  }
+}
 
 const optionsBarCustomOptions = {
   options: {
     plugins: {
       legend: {
-        position: "top",
+        position: 'top',
         labels: {
-          color: "green",
-        },
-      },
+          color: 'green'
+        }
+      }
     },
     scales: {
       x: {
         ticks: {
-          color: "#4285F4",
-        },
+          color: '#4285F4'
+        }
       },
       y: {
         ticks: {
-          color: "#f44242",
-        },
-      },
-    },
-  },
-};
+          color: '#f44242'
+        }
+      }
+    }
+  }
+}
 
-onMounted(() => {
-  initTE({ Chart });
+onMounted(async () => {
+  await initTE({ Chart })
   new Chart(
-    document.getElementById("bar-chart-custom-options"),
+    document.getElementById('bar-chart-custom-options'),
     dataBarCustomOptions,
     optionsBarCustomOptions
-  );
-});
+  )
+})
 </script>
 
 <template>
